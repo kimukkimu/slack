a6949d75e6812f7f8b08b74f771dad032a72c52b
KentaKimura <kentakimura@KentaKimuranoMacBook-Pro.local>
2017-05-11 20:08:17 +0900

    make it better

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 
-#API_TOKEN = "xoxb-181466184758-vwvQStW1hIwP4RH9BjiLHhj1"
+
 API_TOKEN = os.environ.get('SLACKBOT_API','')
 
 default_reply = "Hey, I'm on Heroku"
